2732895055729735726882003e2e15c4caf90a36
Lifeng Li <lifli@cisco.com>
2018-08-24 22:29:23 -0700

    adding basic web UI

--- war/war.js
+++ war/war.js
@@ -8,10 +8,15 @@ class War {
         //current hand
         this.card1 = null;
         this.card2 = null;
+        //equal valued cards that causing a War
+        this.warcard1 = null;
+        this.warcard2 = null;
         //save the pending cards on table
         this.pending = [];
         this.winner = null;
         this.finish = false;
+        this.iswar = false;
+        this.timeout = 2000;
     }
 
     //get card face value
@@ -23,36 +28,44 @@ class War {
     //if one deck is empty, we have a winner
     checker() {
 
-        //testing
-        document.getElementById('deck1').innerHTML = 'Deck1 ' + this.deck1.length;
-        document.getElementById('deck2').innerHTML = 'Deck2 ' + this.deck2.length;
+        //assume we have a winner
+        this.finish = true;
 
+        //worst case: every hand is equal, both decks exhausted, there is no winner
+        if(!this.deck1.length && !this.deck2.length) {
+            this.winner = null;
+        }
         //if deck1 empty, winner is player2
-        if(!this.deck1.length) {
+        else if(!this.deck1.length) {
             this.winner = 'Player 2';
-            this.finish = true;
-            console.log('player 2 wins !!!');
         //if deck2 empty, winner is player1
         } else if(!this.deck2.length) {
             this.winner = 'Player 1';
-            this.finish = true;
-            console.log('player 1 wins !!!');
+        } else {
+            //continue game
+            this.finish = false;
         }
     }
 
     ops(deck, card1, card2) {
+        //winner get all pending cards and current face up cards
         deck.push.apply(deck, this.pending);
         deck.push(card1, card2);
+
         //reset pending list
         this.pending.length = 0;
-        
+
         //check to see if there is a winner
         this.checker();
     }
 
     gotowar(card1, card2) {
+        this.iswar = true;
         //save two equal value cards into pending for winner to pickup
         this.pending.push(card1, card2);
+
+        this.warcard1 = card1;
+        this.warcard2 = card2;
 
         //save next two "face down" cards
         //if both decks have cards, continue the game
@@ -71,17 +84,16 @@ class War {
         this.card1 = this.deck1.shift();
         this.card2 = this.deck2.shift();
         if(this.card1 && this.card2) {
-            this.compare(this.card1, this.card2);
+            setTimeout( () => {
+                this.compare(this.card1, this.card2) },
+                this.timeout
+            );
         } else {
             this.checker();
         }
     }
 
     compare(card1, card2) {
-
-        //testing
-        document.getElementById('card1').innerHTML = 'Card1 ' + this.card1;
-        document.getElementById('card2').innerHTML = 'Card2 ' + this.card2;
 
         let value1 = this.getValue(card1);
         let value2 = this.getValue(card2);
@@ -91,11 +103,13 @@ class War {
 
         //treat 1 as Ace
         } else if(value1 === 1 || ( (value1 > value2) && value2 !== 1 ) ) {
+            this.iswar = false;
             this.ops(this.deck1, card1, card2);
             this.winner = 'Player 1';
 
         //treat 1 as Ace
         } else if(value2 === 1 || value2 > value1 ) {
+            this.iswar = false;
             this.ops(this.deck2, card1, card2);
             this.winner = 'Player 2';
         }
